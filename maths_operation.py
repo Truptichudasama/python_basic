83ab098916e45d8161540e8f1932383dd13ae8a8
Truptichudasama <chudasamatrupti16@gmail.com>
2025-02-27 11:06:13 +0530

    commit

---
+++ maths_operation.py
@@ -0,0 +1,14 @@
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter ssecond number: "))
+
+addition = number1 + number2
+subtract = number1 - number2
+mulitiplication = number1 * number2
+if number2 != 0 :
+    division = number1 / number2
+else:
+    division = "Division by 0 is not possible."
+print("Addition: " , addition)
+print("subtraction: ", subtract)
+print("Multiplication: ", mulitiplication)
+print("Division: ", division)
